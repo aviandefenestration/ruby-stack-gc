695214f794868f2c4bc963ce1e7e39000e09ea98
aviandefenestration <129698542+aviandefenestration@users.noreply.github.com>
2023-07-21 17:38:40 -0400

    Edits to modified benchmark
    - changed some formatting issues

--- fiber_modified.rb
+++ fiber_modified.rb
@@ -1,5 +1,6 @@
 #!/usr/bin/env ruby
 
+
 def recurse(n)
 	# Create an object on the stack to make the GC do a little work:
 	object = Object.new
@@ -21,6 +22,7 @@ def benchmark
 	fiber_full_stack_scan = []
 	thread_full_stack_scan = []
 	stack_scan_bytes = []
+	stack_barrier_met = []
 	15.times do |i|
 		count  = 2**i
 		puts "Count = #{count}"
@@ -30,9 +32,10 @@ def benchmark
 		# The first time we do this, we expect all fibers to be fresh, and we should scan them:
 		GC.start(full_mark: true, immediate_sweep: true)
 		
-		fiber_full_stack_scan << RubyVM.stat.slice(:fiber_full_stack_scan).to_a
-		thread_full_stack_scan << RubyVM.stat.slice(:thread_full_stack_scan).to_a
-		stack_scan_bytes << RubyVM.stat.slice(:stack_scan_bytes).to_a
+		fiber_full_stack_scan.push(RubyVM.stat.slice(:fiber_full_stack_scan).to_a)
+		thread_full_stack_scan.push(RubyVM.stat.slice(:thread_full_stack_scan).to_a)
+		stack_scan_bytes.push(RubyVM.stat.slice(:stack_scan_bytes).to_a)
+		stack_barrier_met.push(RubyVM.stat.slice(:stack_barrier_met).to_a)
 
 		#puts ("Debug counters: " + RubyVM.stat.slice(:fiber_full_stack_scan, :thread_full_stack_scan, :stack_scan_bytes).to_s)
 		#puts ("GC stat: " + GC.stat.slice(:count, :heap_marked_slots, :minor_gc_count, :major_gc_count).to_s)
@@ -41,9 +44,10 @@ def benchmark
 		# The second time we do this, we would imagine that the fiber state has not changed, in theory it should not require any stack scanning:
 		GC.start(full_mark: false, immediate_sweep: true)
 		
-		fiber_full_stack_scan << RubyVM.stat.slice(:fiber_full_stack_scan).to_a
-		thread_full_stack_scan << RubyVM.stat.slice(:thread_full_stack_scan).to_a
-		stack_scan_bytes << RubyVM.stat.slice(:stack_scan_bytes).to_a
+		fiber_full_stack_scan.push(RubyVM.stat.slice(:fiber_full_stack_scan).to_a)
+		thread_full_stack_scan.push(RubyVM.stat.slice(:thread_full_stack_scan).to_a)
+		stack_scan_bytes.push(RubyVM.stat.slice(:stack_scan_bytes).to_a)
+		stack_barrier_met.push(RubyVM.stat.slice(:stack_barrier_met).to_a)
 
 		#puts ("Debug counters: " + RubyVM.stat.slice(:fiber_full_stack_scan, :thread_full_stack_scan, :stack_scan_bytes).to_s)
 		#puts ("GC stat: " + GC.stat.slice(:count, :heap_marked_slots, :minor_gc_count, :major_gc_count).to_s)
@@ -63,13 +67,30 @@ def benchmark
 	
 	a = fiber_full_stack_scan.zip(thread_full_stack_scan).map(&:flatten)
 	a = a.zip(stack_scan_bytes).map(&:flatten)
+	a = a.zip(stack_barrier_met).map(&:flatten)
 
 	count = 1
-	puts "Index	   fiber_full_stack_scan	thread_full_stack_scan	  stack_scan_bytes"
+	#puts "Index	   	fiber scan		thread scan	  stack_scan_bytes  	stack_barrier_met"
+	#a.each do |pair|
+	#	puts [count, pair[1], pair[3], pair[5], pair[7], pair[9]].join("				")
+	#	count += 1
+
+	puts "Index      fiber scan       thread scan      stack_scan_bytes    stack_barrier_met"
 	a.each do |pair|
-		puts [count, pair[1], pair[3], pair[5]].join("			")
+		index = count.to_s.rjust(5)
+		fiber_scan = pair[1].to_s.rjust(15)
+		thread_scan = pair[3].to_s.rjust(15)
+		stack_scan_bytes = pair[5].to_s.rjust(15)
+		stack_barrier_met = pair[7].to_s.rjust(15)
+		puts "#{index}#{fiber_scan}#{thread_scan}#{stack_scan_bytes}#{stack_barrier_met}"
 		count += 1
 	end
+
+	fiber_full_stack_scan = nil
+	thread_full_stack_scan = nil
+	stack_scan_bytes = nil
+	stack_barrier_met = nil
+	a = nil
 
 end
 
